f40c04b1402bdbfda63bb7728c42fa06f535847a
WillV56 <will.vetter56@gmail.com>
2023-04-28 00:39:47 -0700

    new

--- j/main.js
+++ j/main.js
@@ -1,21 +1,36 @@
-fetch("https://thejsway-server.herokuapp.com/api/articles")
-  .then(response => response.json()) // Translate JSON into JavaScript
-  .then(articles => {
-    articles.forEach(article => {
-      // Create title element
-      const titleElement = document.createElement("h3");
-      titleElement.textContent = article.title;
-      // Create content element
-      const contentElement = document.createElement("p");
-      contentElement.textContent = article.content;
-      // Add title and content to the page
-      const articlesElement = document.getElementById("articles");
-      articlesElement.appendChild(titleElement);
-      articlesElement.appendChild(contentElement);
-    });
-  })
-  .catch(err => {
-    console.error(err.message);
-  });
+document.addEventListener('DOMContentLoaded', init);
 
-  // https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1
+function init() {
+
+fetch("https://api.thedogapi.com/v1/images/search?")
+.then(response => response.json())
+.then(dogs => {
+  dogs.forEach(dog => {
+  const urlElement = document.createElement("img");
+  urlElement.src = dog.url;
+  console.log(dog.url);
+  const dogsElement = document.getElementById("dogs");
+  dogsElement.appendChild(urlElement);
+  });
+})
+.catch(err => {
+  console.error(err.message);
+});
+
+
+fetch("https://dogapi.dog/api/v2/facts")
+.then(response => response.json())
+.then(articles => {
+  articles.forEach(article => {
+    const contentElement = document.createElement("p");
+    contentElement.textContent = article.content;
+    console.log(article.content);
+    const articlesElement = document.getElementById("articles");
+    articlesElement.appendChild(contentElement);
+  });
+})
+.catch(err => {
+  console.error(err.message);
+});
+
+}
