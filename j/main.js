b38256ad25ac6a30643a2afb55a6daf253070459
artdm lab <artdm@dvc.edu>
2023-05-02 12:28:40 -0700

    fixed4

--- j/main.js
+++ j/main.js
@@ -8,7 +8,6 @@ fetch("https://api.thedogapi.com/v1/images/search?")
   dogs.forEach(dog => {
   const urlElement = document.createElement("img");
   urlElement.src = dog.url;
-  console.log(dog.url);
   const dogsElement = document.getElementById("dogs");
   dogsElement.appendChild(urlElement);
   });
@@ -21,13 +20,11 @@ fetch("https://api.thedogapi.com/v1/images/search?")
 fetch("https://dogapi.dog/api/v2/facts")
 .then(response => response.json())
 .then(articles => {
-  articles.forEach(article => {
+  const myData = articles.data[0].attributes.body;
     const contentElement = document.createElement("p");
-    contentElement.textContent = article.content;
-    console.log(article.content);
+    contentElement.textContent = myData;
     const articlesElement = document.getElementById("articles");
     articlesElement.appendChild(contentElement);
-  });
 })
 .catch(err => {
   console.error(err.message);
